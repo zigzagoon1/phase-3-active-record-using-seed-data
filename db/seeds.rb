99bfa6cd3268216255a1156dc7d421917add3aa3
KellyMark <kmark602@gmail.com>
2023-05-21 21:04:19 -0400

    commit tree be8e402e2c6f3d5ea0a462ed00e5d66ddd71a241

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,11 @@
 # Add a console message so we can see output when the seed file runs
+puts "Seeding games..."
+
+50.times do 
+    Game.create(
+        title: Faker::Game.title,
+        genre: Faker::Game.genre,
+        platform: Faker::Game.platform,
+        price: rand(0..60)
+    )
+end
